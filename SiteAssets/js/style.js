5cc5d9e499ed8a9ed8b9345486288c3a8232b593
Toukaraafat <tokaraafat25@gmail.com>
2024-10-19 14:46:55 +0200

    commit

--- SiteAssets/js/style.js
+++ SiteAssets/js/style.js
@@ -1,9 +1,9 @@
 $(window).scroll(function () {
-    if ($(document).scrollTop() > 50) {
-        $('header').addClass('scroll')
-    } else {
-        $('header').removeClass('scroll')
-    }
+  if ($(document).scrollTop() > 50) {
+    $('header').addClass('scroll')
+  } else {
+    $('header').removeClass('scroll')
+  }
 })
 /* Open the sidenav */
 function openNav() {
@@ -15,8 +15,41 @@ function closeNav() {
   document.getElementById("sidenav").style.width = "0";
 }
 $(document).ready(function () {
+  $('.partners-carousel').owlCarousel({
+    items: 4,
+    loop: true,
+    dots: false,
+    nav: true,
+    rtl: true,
+    margin: 45,
+    pagination: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    autoplayTimeout: 4000,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+        dots: false
+      },
+      600: {
+        items: 2,
+        nav: true
+      },
+      850: {
+        items: 3,
+        nav: true
+      },
+      1050: {
+        items: 4,
+        nav: true
+      }
 
+    }
+  });
 
+  $(".owl-prev > span").html('<i class="fa fa-chevron-left"></i>');
+  $(".owl-next > span").html('<i class="fa fa-chevron-right"></i>');
 });
 
 
